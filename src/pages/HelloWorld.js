632f13521b10ff2d67da1550a686f0a495a8f6c6
Summer <su943515688@gmail.com>
2018-12-24 01:22:50 +0800

    [+] 增加ant-design-pro组件

--- src/pages/HelloWorld.js
+++ src/pages/HelloWorld.js
@@ -1,15 +1,89 @@
-import {Card} from 'antd'
+import {Charts, Trend} from 'ant-design-pro';
+import {Col, Icon, Row, Tooltip} from 'antd';
+import numeral from 'numeral';
+
+const {ChartCard, yuan, Field} = Charts
+
 
 export default () => {
-    const style = {
-        width: '400px',
-        margin: '30px',
-        boxShadow: '0 4px 8px 0 rgba(0, 0, 0, 0.2)',
-        border: '1px solid #e8e8e8',
-    };
-
     return (
-        <Card style={style} actions={[<a>操作一</a>, <a>操作二</a>]}>
-        </Card>
-    );
+        <Row>
+            <Col span={24}>
+                <ChartCard
+                    title="销售额"
+                    action={
+                        <Tooltip title="指标说明">
+                            <Icon type="info-circle-o"/>
+                        </Tooltip>
+                    }
+                    total={() => (
+                        <span dangerouslySetInnerHTML={{__html: yuan(126560)}}/>
+                    )}
+                    footer={
+                        <Field label="日均销售额" value={numeral(12423).format("0,0")}/>
+                    }
+                    contentHeight={46}
+                >
+        <span>
+          周同比
+          <Trend flag="up" style={{marginLeft: 8, color: "rgba(0,0,0,.85)"}}>
+            12%
+          </Trend>
+        </span>
+                    <span style={{marginLeft: 16}}>
+          日环比
+          <Trend
+              flag="down"
+              style={{marginLeft: 8, color: "rgba(0,0,0,.85)"}}
+          >
+            11%
+          </Trend>
+        </span>
+                </ChartCard>
+            </Col>
+            <Col span={24} style={{marginTop: 24}}>
+                <ChartCard
+                    title="移动指标"
+                    avatar={
+                        <img
+                            style={{width: 56, height: 56}}
+                            src="https://gw.alipayobjects.com/zos/rmsportal/sfjbOqnsXXJgNCjCzDBL.png"
+                            alt="indicator"
+                        />
+                    }
+                    action={
+                        <Tooltip title="指标说明">
+                            <Icon type="info-circle-o"/>
+                        </Tooltip>
+                    }
+                    total={() => (
+                        <span dangerouslySetInnerHTML={{__html: yuan(126560)}}/>
+                    )}
+                    footer={
+                        <Field label="日均销售额" value={numeral(12423).format("0,0")}/>
+                    }
+                />
+            </Col>
+            <Col span={24} style={{marginTop: 24}}>
+                <ChartCard
+                    title="移动指标"
+                    avatar={
+                        <img
+                            alt="indicator"
+                            style={{width: 56, height: 56}}
+                            src="https://gw.alipayobjects.com/zos/rmsportal/dURIMkkrRFpPgTuzkwnB.png"
+                        />
+                    }
+                    action={
+                        <Tooltip title="指标说明">
+                            <Icon type="info-circle-o"/>
+                        </Tooltip>
+                    }
+                    total={() => (
+                        <span dangerouslySetInnerHTML={{__html: yuan(126560)}}/>
+                    )}
+                />
+            </Col>
+        </Row>
+    )
 }
