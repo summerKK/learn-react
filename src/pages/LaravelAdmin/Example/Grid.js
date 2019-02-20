0c7a46adcf9d66b281c48b2f40f76593b0571c82
Summer <su943515688@gmail.com>
2019-02-20 23:27:04 +0800

    [!] form

--- src/pages/LaravelAdmin/Example/Grid.js
+++ src/pages/LaravelAdmin/Example/Grid.js
@@ -1,22 +1,86 @@
 import {Component} from "react"
-import {Affix, Button, Col, Row} from "antd"
+import {Button, Col, DatePicker, Form, Icon, Input, Row} from "antd"
 import styles from "./Grid.less"
 
+const {RangePicker} = DatePicker
+const FormItem = Form.Item
 
+function hasErrors(fieldsError) {
+  return Object.keys(fieldsError).some(filed => fieldsError[filed])
+}
+
+@Form.create()
 export default class Grid extends Component {
+
+  componentDidMount() {
+    this.props.form.validateFields()
+  }
+
+  handleSubmit = (e) => {
+    e.preventDefault()
+    this.props.form.validateFields((err, values) => {
+      if (!err) {
+        console.log('Received values of form:', values)
+      }
+    })
+  }
+
   render() {
+
+    const {
+      getFieldDecorator, getFieldsError, getFieldError, isFieldTouched,
+    } = this.props.form
+
+    const userNameError = isFieldTouched('userName') && getFieldError('userName')
+    const passwordError = isFieldTouched('password') && getFieldError('password')
+
     return (
       <div className={styles.gutterExample}>
-        <Row type="flex" justify="start">
-          <Col span={4} className={styles.gutterRow}>col-4</Col>
-          <Col span={4}>col-4</Col>
-          <Col span={4}>col-4</Col>
-          <Col span={4}>col-4</Col>
+        <Row>
+          <Col span={12}>
+            <RangePicker format="DD/MM/YYYY" showTime/>
+          </Col>
         </Row>
 
-        <Affix offsetTop={120} onChange={affixed => console.log(affixed)}>
-          <Button>120px to affix top</Button>
-        </Affix>,
+        <Row>
+          <Col span={6}>
+            <Form layout="inline" onSubmit={this.handleSubmit}>
+              <FormItem
+                validateStatus={userNameError ? 'error' : 'success'}
+                help={userNameError || ''}
+              >
+                {
+                  getFieldDecorator('userName', {
+                    rules: [{required: true, message: 'Please input you username!'}]
+                  })(
+                    <Input prefix={<Icon type="user" style={{color: 'rgba(0,0,0,.25)'}}/>} placeholder="username"/>
+                  )
+                }
+              </FormItem>
+              <FormItem
+                validateStatus={passwordError ? 'error' : 'success'}
+                help={passwordError || ''}
+              >
+                {
+                  getFieldDecorator('password', {
+                    rules: [{required: true, message: 'Please input you password!'}]
+                  })(
+                    <Input prefix={<Icon type="lock" style={{color: 'rgba(0,0,0,.25)'}}/>} placeholder="password"/>
+                  )
+                }
+              </FormItem>
+              <FormItem>
+                <Button
+                  type="primary"
+                  htmlType="submit"
+                  disabled={hasErrors(getFieldsError())}
+                >
+                  Log in
+                </Button>
+              </FormItem>
+            </Form>
+          </Col>
+        </Row>
       </div>
     )
   }
